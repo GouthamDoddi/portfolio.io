26c2d81ade6acaf057679eb61789e39c06b1a994
GouthamDoddi <gouthambolt@gmail.com>
2020-11-03 16:30:33 +0530

    initial commi

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Layout from './components/grid';
+
+function App() {
+  return (
+    <div className={'root'}>
+        <Layout />
+    </div>
+  );
+}
+
+export default App;
