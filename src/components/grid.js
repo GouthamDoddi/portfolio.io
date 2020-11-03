20f4231d8774ef0330da2fd065ed8dcd224af389
GouthamDoddi <gouthambolt@gmail.com>
2020-11-03 16:52:07 +0530

    initial commit

--- src/components/grid.js
+++ src/components/grid.js
@@ -32,7 +32,7 @@ const Layout = () => {
                 <Grid container spacing={2} fixed className={classes.grid}>
                     <Grid item xs={12} md={6} >
                         {/* <Paper style={{'marginTop':'2em', 'marginLeft':'2em',  'height': '25em', 'width': '90%', 'background': '#e63946' }} id='paper' variant="outlined" square >  */}
-                             <img src="/images/IMG_5910[183] .jpg" alt="boohoo" className="img-responsive" style={{ 'marginTop':'2em',  'marginLeft':'2em', 'height': '90%', 'width': '90%'  }} />
+                             <img src="https://scontent.fvtz2-1.fna.fbcdn.net/v/t31.0-8/28616498_1720256778029865_7749709674862108545_o.jpg?_nc_cat=102&ccb=2&_nc_sid=e3f864&_nc_ohc=IylOtSHkZvAAX_jME2J&_nc_ht=scontent.fvtz2-1.fna&oh=e79252246a808518eece9e495706468e&oe=5FC82F24" alt="boohoo" className="img-responsive" style={{ 'marginTop':'2em',  'marginLeft':'2em', 'height': '90%', 'width': '90%'  }} />
                         {/* </Paper> */}
                     </Grid>
                     <Grid item xs={12} md={6} >
